7572b1087792349cf93af20055e22a00d2975e3a
PetraVidnerova <petra.vidnerova@gmail.com>
2016-04-13 08:41:46 +0200

    save and load model

--- test.py
+++ test.py
@@ -15,5 +15,14 @@ model = RBFNet(10,p=0.1,compute_widths=False)
 model.fit(X,y) 
 yy = model.predict(X)
 
-err = 100*mean_squared_error(y,yy)
+err = 100*mean_squared_error(y, yy)
 print("Err:", err)
+
+from sklearn.externals import joblib 
+
+joblib.dump(model, "rbf.pkl")
+model2 = joblib.load("rbf.pkl") 
+
+yy = model2.predict(X)
+err = 100*mean_squared_error(y, yy)
+print("Err after load:", err) 
