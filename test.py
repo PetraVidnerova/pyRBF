c25e92cafa5562826929d6824f97a8701003c1fd
Petra Vidnerova <petra.vidnerova@gmail.com>
2016-10-12 11:28:04 +0200

    timer

--- test.py
+++ test.py
@@ -2,21 +2,27 @@ import pandas as pd
 import numpy.random as rand 
 from rbf import *
 from sklearn.metrics import mean_squared_error
+from timeit import default_timer as timer 
 
-
-data = pd.read_csv("sin.csv",";") 
+data = pd.read_csv("sin.csv", ";") 
 X = data[data.columns[:-1]] 
 y = data[data.columns[-1]]
 
 X = X.as_matrix()
 y = y.as_matrix()
 
-model = RBFNet(10,p=0.1,compute_widths='none')
-model.fit(X,y) 
-yy = model.predict(X)
+start = timer() 
 
-err = 100*mean_squared_error(y, yy)
-print("Err:", err)
+for i in range(10):
+    model = RBFNet(10, p=0.1, compute_widths='none')
+    model.fit(X,y) 
+    yy = model.predict(X)
+
+    err = 100*mean_squared_error(y, yy)
+    print("Err:", err)
+
+end = timer() 
+print("Time:", end - start) 
 
 from sklearn.externals import joblib 
 
